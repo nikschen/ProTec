1f9e7fb86b588c604c216264bd3d152764507a78
Thom-Mon <thomas.gebel@fh-erfurt.de>
2021-01-06 12:31:33 +0100

    CSS

--- css_Uebungen/FixedHeader.php
+++ css_Uebungen/FixedHeader.php
@@ -41,7 +41,7 @@
   <p>3 Hier könnte quasi eine gesamte Lebensgeschichte stehen und keiner würde es merken...</p>
   <p>4 Hier könnte quasi eine gesamte Lebensgeschichte stehen und keiner würde es merken...</p>
   <p>5 Hier könnte quasi eine gesamte Lebensgeschichte stehen und keiner würde es merken...</p>
-  <p>Hier könnte quasi eine gesamte Lebensgeschichte stehen und keiner würde es merken...</p>
+  <p>Vielleicht sollten wir für die Anzeige auch auf Tabellenelemente setzen oder Flexbox-Elemente</p>
   <p>Hier könnte quasi eine gesamte Lebensgeschichte stehen und keiner würde es merken...</p>
   <p>Hier könnte quasi eine gesamte Lebensgeschichte stehen und keiner würde es merken...</p>
   <p>Hier könnte quasi eine gesamte Lebensgeschichte stehen und keiner würde es merken...</p>
@@ -68,22 +68,65 @@
 
 
  <?php
- echo "php_self";
- echo time();
-echo $_SERVER['PHP_SELF'];
-echo "<br>";
-echo $_SERVER['SERVER_NAME'];
-echo "<br>";
-echo $_SERVER['HTTP_HOST'];
-echo "<br>";
-echo $_SERVER['HTTP_REFERER'];
-echo "<br>";
-echo $_SERVER['HTTP_USER_AGENT'];
-echo "<br>";
-echo $_SERVER['SCRIPT_NAME'];
+
 ?> 
     
-    
+  
+  <footer class="footer">
+  <p>Testtext</p>
+  <table>
+      <tr>
+        <th>Wir babbeln über uns</th>
+        <th>Allgemeine Informationen</th>
+        <th>Service</th>
+        <th>Unsere Partner</th>
+      </tr>
+      <tr>
+        <th>Protec</th>
+        <th><a href="">Zahl-/Versandarten</a></th>
+        <th><a href="">Kunden werben Kunden</a> </th>
+      </tr>
+      <tr>
+        <th>sponsored by PiFoundation</th>
+        <th><a href="">Versand- und Zahlungsbedingungen</a></th>
+        <th><a href="">Treuepunkte</a></th>
+        <th></th>
+      </tr>
+      <tr>
+        <th>Wiemuthgasse 21</th>
+        <th><a href="">Widerrufsrecht</a></th>
+        <th><a href="">Geschäftskundenrabatte</a></th>
+        <th></th>
+      </tr>
+      <tr>
+        <th> 47623 Kevelaer</th>
+        <th><a href="">Datenschutzbestimmungen</a></th>
+        <th><a href="">Newsletter abonnieren</a></th>
+        <th></th>
+      </tr>
+      <tr>
+        <th><br></th>
+        <th><a href="">AGB</a></th>
+        <th><a href="">Kontaktformular</a></th>
+        <th></th>
+      </tr>
+      <tr>
+        <th>Tel: 0500 / 36 36 72 47</th>
+        <th><a href="">Impressum</a></th>
+        <th></th>
+        <th></th>
+      </tr>
+      <tr>
+        <th>info@protec.de</th>
+        <th><a href="">Jobangebote</a></th>
+        <th></th>
+        <th></th>
+      </tr>
+      
+  <table>
+  
+
+  </footer>
 </body>
     
     
