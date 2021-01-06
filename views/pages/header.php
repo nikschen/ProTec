8ac917a9c652b8efa2565a5a7553f820543050f3
Thom-Mon <thomas.gebel@fh-erfurt.de>
2021-01-06 20:04:58 +0100

    updating header

--- views/pages/header.php
+++ views/pages/header.php
@@ -10,17 +10,82 @@
 <body>
    
 <div class="square"></div>
+<div class="border"></div>
 <div class="navbar">
-  
+
   <div class="navigation">
-<ul>
   
-    <img id="slide" src="src/images/LogoProtecTransparent.png" alt="Logo" width="10%" height="10%"  />
-    <li> <a href="./index.html">Elektronik</a> </li>
+<ul>
+    
+    <div class="dropdown">
+        <li class="dropbtn"> <a href="./index.html">Elektronik</a>
+    
+            <div class=dropdown-content>
+                <a href="./index.html">Kabel</a>
+                <a href="./index.html">Werkzeug</a>
+                <a href="./index.html">Verbinder</a>
+                <a href="./index.html">Bauteile</a>
+                <a href="./index.html">Zubehör</a>
+            </div>
+        </li>
+
+    </div>
+    <div class="dropdown">
     <li> <a href="./text-formatieren.html">RaspberryPi</a> </li>
+    
+            <div class=dropdown-content>
+                <a href="./index.html">Pi-2/3/3b/4/4b</a>
+                <a href="./index.html">Pi-Zero</a>
+                <a href="./index.html">Gehäuse</a>
+              
+            </div>
+        </li>
+
+    </div>
+    <div class="dropdown">
     <li> <a href="./bilder-hinzufuegen.html">Computer</a> </li>
+    
+            <div class=dropdown-content>
+                <a href="./index.html">BananaPi</a>
+                <a href="./index.html">Barebones</a>
+                <a href="./index.html">Mainboards</a>
+                <a href="./index.html">Arbeitsspeicher</a>
+            </div>
+        </li>
+
+    </div>
+    <div class="dropdown">
     <li> <a href="./links-erstellen.html">NeuEingetroffen</a> </li>
+    
+            <div class=dropdown-content>
+                <a href="./index.html">Kabel</a>
+                <a href="./index.html">Werkzeug</a>
+                <a href="./index.html">Verbinder</a>
+                <a href="./index.html">Bauteile</a>
+                <a href="./index.html">Zubehör</a>
+            </div>
+        </li>
+
+    </div>
+    <div class="dropdown">
     <li> <a href="./listen-erstellen.html">Sensoren</a> </li>
+    
+            <div class=dropdown-content>
+                <a href="./index.html">Temperatursensoren</a>
+                <a href="./index.html">Bewegungsmelder</a>
+                <a href="./index.html">Strommesser</a>
+                <a href="./index.html">GyroSensor</a>
+            </div>
+        </li>
+
+    </div>
+
+
+
+    
+    
+    
+    
 </ul>
   </div>
 
@@ -34,3 +99,22 @@
   </form> 
 </div>
 </div>
+
+<p>Neue Navigation </p>
+<div class="navigationsContainer"> 
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
