dbcaa8f9af22e98807ae23faa645d89cb0512dd3
nikschen <niklaswiemuth@gmail.com>
2021-01-14 16:24:43 +0100

    businessrabatt umbenannt footer vereinheitlicht

--- views/pages/footer.php
+++ views/pages/footer.php
@@ -4,34 +4,49 @@
       <ul class="footerItems">
         <li>
           <div class="flex-container-content">
-          <p>Wir babbeln über uns</p>
-          <p>ProTec</p>
-          <p>sponsored by PiFoundation</p>
-          <p>Wiemuthgasse 21</p>
-          <p>47623 Kevelaer</p>
-          <p>Tel: 0500 / 36 36 72 47</p>
-          <p>info@protec.de</p>
+              <label class="collapsible">
+                  <input type="checkbox" />
+                  <span class="collapser"><p>Wir babbeln über uns</p></span>
+                  <div class="collapsed">
+                      <p>ProTec</p>
+                      <p>sponsored by PiFoundation</p>
+                      <p>Wiemuthgasse 21</p>
+                      <p>47623 Kevelaer</p>
+                      <p>Tel: 0500 / 36 36 72 47</p>
+                      <p>info@protec.de</p>
+                  </div>
+              </label>
           </div>
         </li>
         <li>
             <div class="flex-container-content">
-              <p>Allgemeine Informationen</p>
-              <a href="index.php?c=pages&a=paymentAndShippingDetails">Versand- und Zahlungsbedingungen</a>
-              <a href="index.php?c=pages&a=rightOfWithdrawal">Widerrufsrecht</a>
-              <a href="index.php?c=pages&a=privacyGuidelines">Datenschutzbestimmungen</a>
-              <a href="index.php?c=pages&a=agb">AGB</a>
-              <a href="index.php?c=pages&a=impressum">Impressum</a>
-              <a href="index.php?c=pages&a=jobs">Jobangebote</a>
+                <label class="collapsible">
+                    <input type="checkbox" />
+                    <span class="collapser"><p>Allgemeine Informationen</p></span>
+                    <div class="collapsed">
+                        <a href="index.php?c=pages&a=paymentAndShippingDetails">Versand- und Zahlungsbedingungen</a>
+                        <a href="index.php?c=pages&a=rightOfWithdrawal">Widerrufsrecht</a>
+                        <a href="index.php?c=pages&a=privacyGuidelines">Datenschutzbestimmungen</a>
+                        <a href="index.php?c=pages&a=agb">AGB</a>
+                        <a href="index.php?c=pages&a=impressum">Impressum</a>
+                        <a href="index.php?c=pages&a=jobs">Jobangebote</a>
+                    </div>
+                </label>
             </div>
         </li>
         <li>
             <div class="flex-container-content">
-              <p>Service</p>
-                <a href="index.php?c=pages&a=customerpromotion">Kunden werben Kunden</a>
-                <a href="index.php?c=pages&a=trustpoints">Treuepunkte</a>
-                <a href="index.php?c=pages&a=businessrabatt">Geschäftskundenrabatte</a>
-                <a href="index.php?c=pages&a=newsletter">Newsletter abonnieren</a>
-                <a href="index.php?c=pages&a=contactform">Kontaktformular</a>
+                <label class="collapsible">
+                    <input type="checkbox" />
+                    <span class="collapser"><p>Service</p></span>
+                    <div class="collapsed">
+                        <a href="index.php?c=pages&a=customerpromotion">Kunden werben Kunden</a>
+                        <a href="index.php?c=pages&a=trustpoints">Treuepunkte</a>
+                        <a href="index.php?c=pages&a=businessdiscount">Geschäftskundenrabatte</a>
+                        <a href="index.php?c=pages&a=newsletter">Newsletter abonnieren</a>
+                        <a href="index.php?c=pages&a=contactform">Kontaktformular</a>
+                    </div>
+                </label>
             </div>
         </li>
         <li>
@@ -40,9 +55,9 @@
                     <input type="checkbox" />
                     <span class="collapser"><p>Unsere Partner</p></span>
                     <div class="collapsed">
-                        <a href=""> <img class="partnerLogo" id="berrybaseLogo" src="<?=ICONSPATH?>partner\berrybase.png" alt="Logo Berrybase" ></a>
-                        <a href=""> <img class="partnerLogo" id="dhlLogo" src="<?=ICONSPATH?>partner\dhllogo.png" alt="Logo DHL" ></a>
-                        <a href=""> <img class="partnerLogo" id="opensourceLogo" src="<?=ICONSPATH?>partner\opensourcelogo.png" alt="Logo OpenSource" ></a>
+                        <a class="partnerLogoAlign" href=""> <img class="partnerLogo" id="berrybaseLogo" src="<?=ICONSPATH?>partner\berrybase.png" alt="Logo Berrybase" ></a>
+                        <a class="partnerLogoAlign" href=""> <img class="partnerLogo" id="dhlLogo" src="<?=ICONSPATH?>partner\dhllogo.png" alt="Logo DHL" ></a>
+                        <a class="partnerLogoAlign" href=""> <img class="partnerLogo" id="opensourceLogo" src="<?=ICONSPATH?>partner\opensourcelogo.png" alt="Logo OpenSource" ></a>
                     </div>
                 </label>
             </div>
