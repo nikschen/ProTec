fd6d031cc66172657e6a50bcad8a9d7f436b4430
nikschen <niklaswiemuth@gmail.com>
2021-01-15 11:03:33 +0100

    formatierung gefixt commonMainPart für Hauptteil der Seite nutzen oder kopieren!

--- views/pages/categoryRaspi.php
+++ views/pages/categoryRaspi.php
@@ -1,29 +1,23 @@
-<link rel="stylesheet" href="assets/css/registration.css">
 <body class="Site">
-<h3>Wählen Sie eine Kategorie</h3>
-<main class="Site-content">
-    
-    
-    <div class="contentWrapper">
-        <div class="element">
-        <a href="index.php?c=pages&a=categoryRaspi">
-        <img src="src\images\19.png" alt="RaspberryPi Abbildung" >
-        <p>RaspberryPi-Boards von One bis Zero</p></a>
-        </div>
-        <div class="element">
-        <a href="index.php?c=pages&a=categoryRaspi">
-        <img src="src\images\28.png" alt="RaspberryPi Gehäuse" >
-        <p>RaspberryPi-Gehäuse für alle Boards</p></a>
-        </div>
-        
-    </div>
 
-  
-      
-          
-  
-        
-    
-</main>
+    <main class="Site-content">
+        <h3>Wählen Sie eine Kategorie</h3>
+
+        <div class="contentWrapper">
+            <div class="element">
+                <a href="index.php?c=pages&a=categoryRaspi">
+                    <img src="src\images\19.png" alt="RaspberryPi Abbildung">
+                    <p>RaspberryPi-Boards von One bis Zero</p></a>
+            </div>
+            <div class="element">
+                <a href="index.php?c=pages&a=categoryRaspi">
+                    <img src="src\images\28.png" alt="RaspberryPi Gehäuse">
+                    <p>RaspberryPi-Gehäuse für alle Boards</p></a>
+            </div>
+
+        </div>
+
+
+    </main>
 </body>
 
