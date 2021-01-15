ae2ad4f3811bdf4efb62b205f990309e1301b55c
nikschen <niklaswiemuth@gmail.com>
2021-01-15 12:30:05 +0100

    css

--- views/accounts/signup.php
+++ views/accounts/signup.php
@@ -1,11 +1,3 @@
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/registration.css">
-    <script type="text/javascript" src="assets\js\registerform.js"></script>
-</head>
-
 <body>
     <?php if(isset($errors) && count($errors) > 0) : ?>
      <div class="error-message" style ="border: 1px dottet red">
