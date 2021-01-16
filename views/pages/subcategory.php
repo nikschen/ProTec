33067be59647417ac9dc4428b68e60c9efd70da9
nikschen <niklaswiemuth@gmail.com>
2021-01-16 11:59:46 +0100

    zentrierung der Artikel

--- views/pages/subcategory.php
+++ views/pages/subcategory.php
@@ -1,23 +1,20 @@
 <body class="Site">
 <main class="Site-content">
     <h3><?=$category?></h3>
-
-        <div class="subcategoryContent">
-        <?foreach($products as $key=>$value)
-            {
-                $prodID=$value['productID'];
-                $prodName=$value['prodName'];?>
-
+        <div class="contentContainer">
+            <div class="subcategoryContent">
+            <?foreach($products as $key=>$value)
+                {
+                    $prodID=$value['productID'];
+                    $prodName=$value['prodName'];?>
 
                     <div class="element">
                         <a href="index.php?c=pages&a=product&pid=<?=$prodID?>">
-                            <img src="\src\images\<?=$prodID?>.png">
-                            <p><?=$prodName?></p></a>
+                        <img src="\src\images\<?=$prodID?>.png">
+                        <p><?=$prodName?></p></a>
                     </div>
-
-
-
-        <?}?>
+            <?}?>
+            </div>
         </div>
 </main>
 </body>
