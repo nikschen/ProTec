06d1c13d014d55c8a7eb5b8bd886d26a571fecfd
nikschen <niklaswiemuth@gmail.com>
2021-01-18 21:29:53 +0100

    braucht noch pflege

--- views/pages/head.php
+++ views/pages/head.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/staticGlobalVariables.css">
     <link rel="stylesheet" href="assets/css/header.css">
     <link rel="stylesheet" href="assets/css/commonMainPart.css">
     <link rel="stylesheet" href="assets/css/footer.css">
