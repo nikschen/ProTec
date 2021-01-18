160510303f7bd604aeffe4b83ae03bb4e6d7fee3
nikschen <niklaswiemuth@gmail.com>
2021-01-18 16:44:11 +0100

    variablen für hauptfarbe und akzentfarbe angelegt hintergrund hinzugefügt (nicht final) hover effekt homepage

--- views/pages/index.php
+++ views/pages/index.php
@@ -5,20 +5,20 @@
                 <ul class="leftHomeColumnElements">
                     <div class="leftHomeColumnUpperPartContainer">
                         <ul class="leftHomeColumnUpperPartElements">
-                            <li>
+                            <li class="exampleImageLeftUpperCorner">
                                 <a href="index.php?c=pages&a=subcategory&subcat=Computer&cat=Komplett-PCs"><img src="<?=IMAGESPATH?>37.png" alt="Komplett-PC Beispielbild"></a>
                             </li>
-                            <li>
+                            <li class="exampleImageLeftUpperMiddle">
                                 <a href="index.php?c=pages&a=subcategory&subcat=RaspberryPi&cat=Gehäuse"><img src="<?=IMAGESPATH?>29.png" alt="RaspberryPI Gehäuse Beispielbild"></a>
                             </li>
                         </ul>
                     </div>
                     <div class="leftHomeColumnLowerPartContainer">
                         <ul class="leftHomeColumnUpperPartElements">
-                            <li>
+                            <li class="exampleImageLeftLowerMiddle">
                                 <a href="index.php?c=pages&a=subcategory&subcat=RaspberryPi&cat=Pi-1/2/3/4/Zero"><img src="<?=IMAGESPATH?>24.png" alt="RaspberryPi Beispielbild"></a>
                             </li>
-                            <li>
+                            <li class="exampleImageLeftLowerCorner">
                                 <a href="index.php?c=pages&a=subcategory&subcat=Sensoren&cat=Strommesser"><img src="<?=IMAGESPATH?>50.png" alt="Strommesser Beispielbild"></a>
                             </li>
                         </ul>
@@ -31,10 +31,10 @@
                     <li>
                         <div class="middleHomeColumnUpperPartContainer">
                             <ul class="middleHomeColumnUpperPartElements">
-                                <li>
+                                <li class="exampleImageMiddleUpperLeft">
                                     <a href="index.php?c=pages&a=subcategory&subcat=Computer&cat=Arbeitsspeicher"><img src="<?=IMAGESPATH?>41.png" alt="Arbeitsspeicher Beispielbild"></a>
                                 </li>
-                                <li>
+                                <li class="exampleImageMiddleUpperRight">
                                     <a href="index.php?c=pages&a=subcategory&subcat=Elektronik&cat=Kabel"><img src="<?=IMAGESPATH?>1.png" alt="Kabel Beispielbild"></a>
                                 </li>
                             </ul>
@@ -48,10 +48,10 @@
                     <li>
                         <div class="middleHomeColumnLowerPartContainer">
                             <ul class="middleHomeColumnUpperPartElements">
-                                <li>
+                                <li class="exampleImageMiddleLowerLeft">
                                     <a href="index.php?c=pages&a=subcategory&subcat=Sensoren&cat=Bewegungsmelder"><img src="<?=IMAGESPATH?>47.png" alt="Bewegungsmelder Beispielbild"></a>
                                 </li>
-                                <li>
+                                <li class="exampleImageMiddleLowerRight">
                                     <a href="index.php?c=pages&a=subcategory&subcat=Sensoren&cat=Temperatursensoren"><img src="<?=IMAGESPATH?>45.png" alt="Temperatursensor Beispielbild"></a>
                                 </li>
                             </ul>
@@ -65,20 +65,20 @@
                 <ul class="rightHomeColumnElements">
                     <div class="rightHomeColumnUpperPartContainer">
                         <ul class="rightHomeColumnUpperPartElements">
-                            <li>
+                            <li class="exampleImageRightUpperCorner">
                                 <a href="index.php?c=pages&a=subcategory&subcat=Elektronik&cat=Werkzeug"><img src="<?=IMAGESPATH?>5.png" alt="Werkzeug Beispielbild"></a>
                             </li>
-                            <li>
+                            <li class="exampleImageRightUpperMiddle">
                                 <a href="index.php?c=pages&a=subcategory&subcat=Computer&cat=Barebones"><img src="<?=IMAGESPATH?>33.png" alt="Barebones Beispielbild"></a>
                             </li>
                         </ul>
                     </div>
                     <div class="rightHomeColumnLowerPartContainer">
                         <ul class="rightHomeColumnUpperPartElements">
-                            <li>
+                            <li class="exampleImageRightLowerMiddle">
                                 <a href="index.php?c=pages&a=subcategory&subcat=Elektronik&cat=Bauteile"><img src="<?=IMAGESPATH?>10.png" alt="Bauteile Beispielbild"></a>
                             </li>
-                            <li>
+                            <li class="exampleImageRightLowerCorner">
                                 <a href="index.php?c=pages&a=categoryNew"><img src="<?=IMAGESPATH?>32.png" alt="Neu Beispielbild"></a>
                             </li>
                         </ul>
