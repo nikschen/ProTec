350515e718d04d551105eda68a9d75ef097ffc95
nikschen <niklaswiemuth@gmail.com>
2021-02-02 16:32:12 +0100

    header fix + admin weiter ausgebaut

--- views/administrativeOperations/chooseOperation.php
+++ views/administrativeOperations/chooseOperation.php
@@ -2,12 +2,12 @@
 <main class="Site-content">
     <div class="administrationContentWrapper">
         <div class="element">
-            <a href="index.php?c=administrativeOperations&a=">
+            <a href="index.php?c=administrativeOperations&a=manageProducts">
                 <img src="<?=ICONSPATH?>categoriesUnited.png" alt="alle normalen Kategorien">
                 <p>Produkte verwalten</p></a>
         </div>
         <div class="element">
-            <a href="index.php?c=administrativeOperations&a=">
+            <a href="index.php?c=administrativeOperations&a=manageCustomers">
                 <img src="<?=ICONSPATH?>userIcon.png" alt="User Symbol">
                 <p>Kunden verwalten</p></a>
         </div>
