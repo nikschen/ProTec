f6a0eb9b8d794c2eeb6e361f72e83e8962f8e747
Thom-Mon <thomas.gebel@fh-erfurt.de>
2021-02-09 10:30:03 +0100

    Fehleranzeige Profile JS PHP Implementiert

--- controller/accountsController.php
+++ controller/accountsController.php
@@ -26,6 +26,97 @@ class AccountsController extends \protec\core\Controller
 		{
 			echo "<hr> "."<br>" . "Submitted";
 			//exit(0);
+		$email = $_POST['email'] ?? null; //
+		$password = $_POST['password'] ?? null;//
+		$password_repeat = $_POST['password-repeat'] ?? null;
+		$telefon= $_POST['fon'] ?? null;//
+		$title = $_POST['title'] ?? null;//
+		$firstName = $_POST['firstName'] ?? null;//
+		$lastName = $_POST['lastName'] ?? null;// check warum ab Leerzeichen getrennt wird
+		$birthDate = $_POST['birthDate'] ?? null;
+		$salutation = $_POST['Anrede'] ?? null; //
+
+		$streetInfo= $_POST['streetInfo'] ?? null; //
+		$streetNo= $_POST['streetNo'] ?? null;//
+		$address2= $_POST['address2'] ?? null; //eventuell rauslassen (Mit Niklasius besprechen) 
+		$zipcode= $_POST['zipcode'] ?? null;//
+		$city= $_POST['city'] ?? null;//
+		$country= $_POST['country'] ?? null;//
+
+		//echo print_r($_POST,true);
+		if(mb_strlen($firstName)<2 || mb_strlen($firstName)>46 || preg_match('/[0-9]/',$firstName))
+		{
+			$errors['firstName'] = "Vorname entspricht nicht den Anforderungen -> mind. 2 max. 46 Zeichen,  keine Zahlen oder Sonderzeichen";
+		}
+	
+		if(mb_strlen($lastName)<2 || mb_strlen($firstName)>100 ||preg_match('/[0-9]/',$lastName))
+		{
+			$errors['lastName'] = "Name entspricht nicht den Anforderungen -> min. 2 max. 100 Zeichen, keine Zahlen oder Sonderzeichen";
+		}
+	
+		if(mb_strlen($birthDate)<10 || mb_strlen($birthDate)>15 || !preg_match('/^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$/',$birthDate))
+		{
+			$errors['birthDate'] = "Geburtsdatum fehlerhaft: Das ist kein gültiges Datumsformat. Bsp: 12. März 2020 = 12.03.2020";
+		}
+	
+		if(mb_strlen($title)>0 && mb_strlen($title)<2)
+		{
+			$errors['title'] = "Titel zu kurz";
+		}
+	
+		if(mb_strlen($password)<8 || !preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_-])[A-Za-z\d@$!%*?&_-]{8,}$/',$password))
+		{
+			$errors['password'] = "Password unzureichend. Anforderungen: min. 8 Zeichen, min. 1 Klein- und Großbuchstaben, min. 1 Sonderzeichen (@$!%*?&_-)";
+		}
+	
+		if($password !== $password_repeat)
+		{
+			$errors['password-ident'] = "Die Passworteingaben sind leider nicht identisch!";
+		}
+
+		if(mb_strlen($email)<4 || !filter_var($email, FILTER_VALIDATE_EMAIL))
+		{
+			$errors['email'] = "Email entspricht nicht den Vorgaben";
+		}
+
+		if( !is_numeric($telefon))
+		{
+			$errors['fon'] = "Bitte geben Sie Ihre Telefonnummer ohne Sonderzeichen/Leerzeichen an";
+		}
+
+		if(mb_strlen($streetInfo)<2 || mb_strlen($streetInfo)>255 )
+		{
+			$errors['streetInfo'] = "Straßenangabe entspricht nicht den Anforderungen min. 2 max. 255 Zeichen";
+		}
+
+		if(mb_strlen($streetNo)<1 || mb_strlen($streetNo)>10 )
+		{
+			$errors['streetNo'] = "Hausnummereingabe entspricht nicht den Anforderungen min. 1 max. 10 Zeichen";
+		}
+		if(mb_strlen($zipcode)<3 || mb_strlen($zipcode)>12 )
+		{
+			$errors['zipcode'] = "PLZ entspricht nicht den Anforderungen min. 3 max. 12 Zeichen";
+		}
+		if(mb_strlen($city)<2 || mb_strlen($city)>60 )
+		{
+			$errors['city'] = "Stadt entspricht nicht den Anforderungen min. 2 max. 60 Zeichen";
+		}
+		
+		if(mb_strlen($country)<2 || mb_strlen($country)>60 )
+		{
+			$errors['country'] = "Land entspricht nicht den Anforderungen min. 2 max. 60 Zeichen";
+			
+		}
+		$this->setParam('errors', $errors);
+		
+		
+
+
+		if(count($errors)===0)
+		{
+		}
+
+		//exit(0);
 		}
 
 	}
@@ -65,7 +156,7 @@ class AccountsController extends \protec\core\Controller
 		$zipcode= $_POST['zipcode'] ?? null;//
 		$city= $_POST['city'] ?? null;//
 		$country= $_POST['country'] ?? null;//
-
+		
 		
 		
 	
--- views/accounts/profile.php
+++ views/accounts/profile.php
@@ -3,9 +3,19 @@
 
 <div class="registrationForm">
 
+<?php if(isset($errors) && count($errors) > 0) : ?>
+     <div class="error-message" style ="border-radius:5px;background-color: red; color: white">
+     <ul>
+         <?php foreach($errors as $key => $value) : ?>
+            <li><?=$value?></li>
+            <?php endforeach?>
+         </ul>
+         </div>
+         <?php endif; ?>
 <?
+   
     $customerTable = getUserInformation($_SESSION['email']); //initializes the logged in user with all his required infos
-
+    
     
 
 
@@ -25,20 +35,20 @@
 
             <input type="text" name="title" placeholder="Titel" value="Titel noch nicht in DB"><br>
 
-            <input type="text" name="lastName" placeholder="Ihr Name*" value="<?=htmlspecialchars($customerTable[0]['lastName']);?>"><br>
-            <input type="text" name="firstName" placeholder="Ihr Vorname*" value="<?=htmlspecialchars($customerTable[0]['firstName']);?>"><br>
-            <input type="text" class="birthDateToCheck" name="birthDate" placeholder="Geburtsdatum*" onchange='checkBirthDate()' value="<?=htmlspecialchars($customerTable[0]['birthDate']);?>"><br>
+            <input type="text" name="lastName" placeholder="Ihr Name*" value="<?if(isset($_POST['lastName'])){echo $_POST['lastName'];}else{echo htmlspecialchars($customerTable[0]['lastName']);}?>"><br>
+            <input type="text" name="firstName" placeholder="Ihr Vorname*" value="<?if(isset($_POST['firstName'])){echo $_POST['firstName'];}else{echo htmlspecialchars($customerTable[0]['firstName']);}?>"><br>
+            <input type="text" class="birthDateToCheck" name="birthDate" placeholder="Geburtsdatum*" onchange='checkBirthDate()' value="<?if(isset($_POST['birthDate'])){echo $_POST['birthDate'];}else{echo htmlspecialchars($customerTable[0]['birthDate']);}?>"><br>
 
             <p class="messageDate"></p>
 
-            <input type="text" class="emailToCheck" name="email" placeholder="Ihre E-Mail-Adresse*" onchange='checkEmail()' value="<?=htmlspecialchars($customerTable[0]['eMail']);?>"><br>
+            <input type="text" class="emailToCheck" name="email" placeholder="Ihre E-Mail-Adresse*" onchange='checkEmail()' value="<?if(isset($_POST['eMail'])){echo $_POST['eMail'];}else{echo htmlspecialchars($customerTable[0]['eMail']);}?>"><br>
 
             <p class="messageMail"></p>
 
-            <input type="text" name="fon" placeholder="Telefon oder Mobilnummer:" value="<?=htmlspecialchars($customerTable[0]['phone']);?>"><br>
+            <input type="text" name="fon" placeholder="Telefon oder Mobilnummer:" value="<?if(isset($_POST['phone'])){echo $_POST['phone'];}else{echo htmlspecialchars($customerTable[0]['phone']);}?>"><br>
             
-            <input type="password" class="password1" name="password" placeholder="Ihr Passwort*" onchange='checkPassword()'required>
-            <input type="password" class="password2" name="password-repeat" placeholder="Ihr Passwort wiederholt*" onchange='checkPassword()'required><br>
+            <input type="password" class="password1" name="password" placeholder="Ihr Passwort*" onchange='checkPassword()'>
+            <input type="password" class="password2" name="password-repeat" placeholder="Ihr Passwort wiederholt*" onchange='checkPassword()'><br>
 
             <p class="messagePassword"></p>
         
@@ -46,12 +56,12 @@
             <h2>Ihre Adresse</h2>
             <hr>
 
-            <input type="text" class="streetInfo" name="streetInfo" placeholder="Straße*" value="<?=htmlspecialchars($customerTable[0]['street']);?>"><br>
-            <input class="streetNo" type="text" name="streetNo" placeholder="Hausnummer*" value="<?=htmlspecialchars($customerTable[0]['streetNumber']);?>"><br>
-            <input type="text" name="address2" placeholder="Adresszusatz" value="<?=htmlspecialchars($customerTable[0]['additionalInformation']);?>"><br>
-            <input class="oneLine" type="text" name="zipcode" placeholder="PLZ*" value="<?=htmlspecialchars($customerTable[0]['zipCode']);?>">
-            <input class="oneLine" type="text" name="city" placeholder="Ort*" value="<?=htmlspecialchars($customerTable[0]['city']);?>"><br>
-            <input type="text" name="country" placeholder="Land*" value="<?=htmlspecialchars($customerTable[0]['country']);?>"><br><br>
+            <input type="text" class="streetInfo" name="streetInfo" placeholder="Straße*" value="<?if(isset($_POST['streetInfo'])){echo $_POST['streetInfo'];}else{echo htmlspecialchars($customerTable[0]['street']);}?>"><br>
+            <input class="streetNo" type="text" name="streetNo" placeholder="Hausnummer*" value="<?if(isset($_POST['streetNo'])){echo $_POST['streetNo'];}else{echo htmlspecialchars($customerTable[0]['streetNumber']);}?>"><br>
+            <input type="text" name="address2" placeholder="Adresszusatz" value="<?if(isset($_POST['address2'])){echo $_POST['address2'];}else{echo htmlspecialchars($customerTable[0]['additionalInformation']);}?>"><br>
+            <input class="oneLine" type="text" name="zipcode" placeholder="PLZ*" value="<?if(isset($_POST['zipcode'])){echo $_POST['zipcode'];}else{echo htmlspecialchars($customerTable[0]['zipCode']);}?>">
+            <input class="oneLine" type="text" name="city" placeholder="Ort*" value="<?if(isset($_POST['city'])){echo $_POST['city'];}else{echo htmlspecialchars($customerTable[0]['city']);}?>"><br>
+            <input type="text" name="country" placeholder="Land*" value="<?if(isset($_POST['country'])){echo $_POST['country'];}else{echo htmlspecialchars($customerTable[0]['country']);}?>"><br><br>
             <p>Die mit * markierten Felder sind Pflichtfelder</p>
             <input type="submit" name="submit" value="Absenden"></input>
         </form>
