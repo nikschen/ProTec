b062390e34352a68f746b68efd11fb66e9b83655
nikschen <niklaswiemuth@gmail.com>
2021-02-12 16:42:17 +0100

    schattenbearbeitet, schatten mit border ausgetauscht nach kruses empfehlung

--- views/pages/index.php
+++ views/pages/index.php
@@ -1,68 +1,70 @@
 <body class="Site">
     <main class="Site-content">
         <div class="homepageContainer">
+
             <div class="homeContainer">
-            <div class="leftHomeColumnContainer">
-                <ul class="leftHomeColumnElements">
-                    <div class="leftHomeColumnUpperPartContainer">
-                        <ul class="leftHomeColumnUpperPartElements">
-                            <li class="exampleImageLeftUpperCorner">
-                                <a href="index.php?c=pages&a=subcategory&subcat=Computer&cat=Komplett-PCs"><img src="<?=IMAGESPATH?>37.png" alt="Komplett-PC Beispielbild"></a>
+                <div class="homeContainerInnerBorder">
+                    <div class="leftHomeColumnContainer">
+                        <ul class="leftHomeColumnElements">
+                            <div class="leftHomeColumnUpperPartContainer">
+                                <ul class="leftHomeColumnUpperPartElements">
+                                    <li class="exampleImageLeftUpperCorner">
+                                        <a href="index.php?c=pages&a=subcategory&subcat=Computer&cat=Komplett-PCs"><img src="<?=IMAGESPATH?>37.png" alt="Komplett-PC Beispielbild"></a>
+                                    </li>
+                                    <li class="exampleImageLeftUpperMiddle">
+                                        <a href="index.php?c=pages&a=subcategory&subcat=RaspberryPi&cat=Gehäuse"><img src="<?=IMAGESPATH?>29.png" alt="RaspberryPI Gehäuse Beispielbild"></a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="leftHomeColumnLowerPartContainer">
+                                <ul class="leftHomeColumnUpperPartElements">
+                                    <li class="exampleImageLeftLowerMiddle">
+                                        <a href="index.php?c=pages&a=subcategory&subcat=RaspberryPi&cat=Pi-1/2/3/4/Zero"><img src="<?=IMAGESPATH?>24.png" alt="RaspberryPi Beispielbild"></a>
+                                    </li>
+                                    <li class="exampleImageLeftLowerCorner">
+                                        <a href="index.php?c=pages&a=subcategory&subcat=Sensoren&cat=Strommesser"><img src="<?=IMAGESPATH?>50.png" alt="Strommesser Beispielbild"></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </ul>
+                    </div>
+
+                    <div class="middleHomeColumnContainer">
+                        <ul class="middleHomeColumnElements">
+                            <li>
+                                <div class="middleHomeColumnUpperPartContainer">
+                                    <ul class="middleHomeColumnUpperPartElements">
+                                        <li class="exampleImageMiddleUpperLeft">
+                                            <a href="index.php?c=pages&a=subcategory&subcat=Computer&cat=Arbeitsspeicher"><img src="<?=IMAGESPATH?>41.png" alt="Arbeitsspeicher Beispielbild"></a>
+                                        </li>
+                                        <li class="exampleImageMiddleUpperRight">
+                                            <a href="index.php?c=pages&a=subcategory&subcat=Elektronik&cat=Kabel"><img src="<?=IMAGESPATH?>1.png" alt="Kabel Beispielbild"></a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </li>
-                            <li class="exampleImageLeftUpperMiddle">
-                                <a href="index.php?c=pages&a=subcategory&subcat=RaspberryPi&cat=Gehäuse"><img src="<?=IMAGESPATH?>29.png" alt="RaspberryPI Gehäuse Beispielbild"></a>
+                            <li>
+                                <div class="middleHomeColumnMiddlePartContainer">
+                                <img src="<?=ICONSPATH?>protecSquare.png" alt="ProTec Logo">
+                                </div>
+                            </li>
+                            <li>
+                                <div class="middleHomeColumnLowerPartContainer">
+                                    <ul class="middleHomeColumnUpperPartElements">
+                                        <li class="exampleImageMiddleLowerLeft">
+                                            <a href="index.php?c=pages&a=subcategory&subcat=Sensoren&cat=Bewegungsmelder"><img src="<?=IMAGESPATH?>47.png" alt="Bewegungsmelder Beispielbild"></a>
+                                        </li>
+                                        <li class="exampleImageMiddleLowerRight">
+                                            <a href="index.php?c=pages&a=subcategory&subcat=Sensoren&cat=Temperatursensoren"><img src="<?=IMAGESPATH?>45.png" alt="Temperatursensor Beispielbild"></a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </li>
                         </ul>
                     </div>
-                    <div class="leftHomeColumnLowerPartContainer">
-                        <ul class="leftHomeColumnUpperPartElements">
-                            <li class="exampleImageLeftLowerMiddle">
-                                <a href="index.php?c=pages&a=subcategory&subcat=RaspberryPi&cat=Pi-1/2/3/4/Zero"><img src="<?=IMAGESPATH?>24.png" alt="RaspberryPi Beispielbild"></a>
-                            </li>
-                            <li class="exampleImageLeftLowerCorner">
-                                <a href="index.php?c=pages&a=subcategory&subcat=Sensoren&cat=Strommesser"><img src="<?=IMAGESPATH?>50.png" alt="Strommesser Beispielbild"></a>
-                            </li>
-                        </ul>
-                    </div>
-                </ul>
-            </div>
-
-            <div class="middleHomeColumnContainer">
-                <ul class="middleHomeColumnElements">
-                    <li>
-                        <div class="middleHomeColumnUpperPartContainer">
-                            <ul class="middleHomeColumnUpperPartElements">
-                                <li class="exampleImageMiddleUpperLeft">
-                                    <a href="index.php?c=pages&a=subcategory&subcat=Computer&cat=Arbeitsspeicher"><img src="<?=IMAGESPATH?>41.png" alt="Arbeitsspeicher Beispielbild"></a>
-                                </li>
-                                <li class="exampleImageMiddleUpperRight">
-                                    <a href="index.php?c=pages&a=subcategory&subcat=Elektronik&cat=Kabel"><img src="<?=IMAGESPATH?>1.png" alt="Kabel Beispielbild"></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="middleHomeColumnMiddlePartContainer">
-                        <img src="<?=ICONSPATH?>protecSquare.png" alt="ProTec Logo">
-                        </div>
-                    </li>
-                    <li>
-                        <div class="middleHomeColumnLowerPartContainer">
-                            <ul class="middleHomeColumnUpperPartElements">
-                                <li class="exampleImageMiddleLowerLeft">
-                                    <a href="index.php?c=pages&a=subcategory&subcat=Sensoren&cat=Bewegungsmelder"><img src="<?=IMAGESPATH?>47.png" alt="Bewegungsmelder Beispielbild"></a>
-                                </li>
-                                <li class="exampleImageMiddleLowerRight">
-                                    <a href="index.php?c=pages&a=subcategory&subcat=Sensoren&cat=Temperatursensoren"><img src="<?=IMAGESPATH?>45.png" alt="Temperatursensor Beispielbild"></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                </ul>
-            </div>
 
 
-            <div class="rightHomeColumnContainer">
+                    <div class="rightHomeColumnContainer">
                 <ul class="rightHomeColumnElements">
                     <div class="rightHomeColumnUpperPartContainer">
                         <ul class="rightHomeColumnUpperPartElements">
@@ -86,7 +88,8 @@
                     </div>
                 </ul>
             </div>
-        </div>
+                </div>
+            </div>
         </div>
     </main>
 </body>
