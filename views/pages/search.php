af2b76ba48173bb341af15e3f7942e0704c8f627
Thom-Mon <thomas.gebel@fh-erfurt.de>
2021-02-17 20:42:03 +0100

    Suchfilter verbessern

--- views/pages/search.php
+++ views/pages/search.php
@@ -1,5 +1,27 @@
 <body class="Site">
     <main class="Site-content">
+
+<div class="filterOptions" id="filter" style=color:black;>
+ 
+    <?//Diese Anzeige dient der Auswahl einer Kategorie nachdem die Suchanfrage dies bereits eingeschränkt hat?>
+    <?$categoriesInSearch=[];          ?>
+    <?foreach($products as $element) : ?>
+        <?if(in_array($element->category,$categoriesInSearch)){}else{array_push($categoriesInSearch,$element->category);}  ?>
+    <?endforeach;?> 
+    <?foreach($categoriesInSearch as $element) : ?>
+    <input class="categorieFilter" type="checkbox" name="<?=$element?>"><?=$element?><br>
+    <?endforeach;?>
+    <form method="post">
+        <label for="minPrice">Preis</label>  
+        <input type="text" name="minPrice" placeholder="von">
+        <input type="text" name="maxPrice" placeholder="bis"><br>
+        <button type="submit" name="searchString">Ergebnisse filtern</button><br>
+    </form>
+</div>
+
+
+
+
         <div class="subCategoryContentContainer">
 
        
@@ -20,7 +42,11 @@
                 <div class="searchResults-message" >
                     <br>
                     <p>Ihre Suchanfrage ergab leider keine Treffer</p><br>
-                    <img src="<?=IMAGESPATH?>sadrobot1.png" alt="sadrobot">
+                    <img src="<?=IMAGESPATH?>sadrobot1.png" alt="sadrobot"><br>
+                    <p>Halb so schlimm...starten Sie einfach eine neue Suche...</p><br>
+                    <form method="POST">
+                    <input type="text" name="searchString" placeholder="neue Suche..."<?if(isset($_POST['searchString'])){echo "value=".htmlspecialchars($_POST['searchString']);};?>>
+                    </form>
                 </div>
             <?endif;?>
             
