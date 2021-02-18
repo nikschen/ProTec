c2f5e8d044de16fd799499d825e4ed57d505e8ad
nikschen <niklaswiemuth@gmail.com>
2021-02-18 16:07:08 +0100

    docutime!

--- views/servicepages/documentation.php
+++ views/servicepages/documentation.php
@@ -4,7 +4,7 @@
         <h2>Projektdokumentation ProTec - ein GWP/DWP-Projekt</h2>
         <ol>
             <li>
-                <h3>Einleitung</h3>
+                <h3>Projektbeschreibung</h3>
                 <p>
                     Im Rahmen der Module Dynamische Webprogrammierung und Grundlagen Webprogrammierung soll eine Webseite für einen Online-Shop entworfen und implementiert werden. Im Rahmen der
                     Themenfindung wurde ein Webshop für Elektronik, Computer, Computerteile und RaspberryPi sowie zugehörige Komponenten gewählt. Dabei ist es nach Anmeldung möglich verschiedenste
@@ -13,21 +13,70 @@
                 </p>
             </li>
             <li>
-                <h3>Projektbeschreibung</h3>
-            </li>
-            <li>
                 <h3>Anforderungsbeschreibung</h3>
-                <ol>
-                    <li><h4>Muss-Kriterien</h4></li>
-                    <li><h4>Wunsch-Kriterien</h4></li>
-                </ol>
+                <h4>Muss-Kriterien</h4>
+                <ul>
+                    <li>
+                        Allgemein
+                        <ul>
+
+                            <li>Webseitenstruktur mit mindestens 6 Seiten und 3 Unterseiten</li>
+                            <li>einheitliche Navigation mit Untermenüs über das gesamte Webportal</li>
+                            <li>mindestens 3 Formulare (Login, Registrierung und Kontaktformulare)</li>
+                            <li>Datenbankanbindung mit Beispieldaten</li>
+                            <li>Schreib- und Lese-Zugriff auf Datenbank</li>
+                        </ul>
+                    </li>
+                    <li>
+                        GWP
+                        <ul>
+                            <li>passende Gestaltung dem Thema entsprechend</li>
+                            <li>Barrierefreiheit durch Verwendung von Meta, Alt, Title Tags</li>
+                            <li>Einbindung von Bildern</li>
+                            <li>Gliederung durch HTML-Elemente wie div, nav, header, article, section</li>
+                            <li>responsives Webdesign (mobile-friendly und Desktop-konform)</li>
+                        </ul>
+                    </li>
+                    <li>
+                        DWP
+                        <ul>
+                            <li>Behandlung und Validierung von Formularen mittels PHP serverseitig und Javascript
+                                clientseitig
+                            </li>
+                            <li>dynamischer Aufbau von Seiten anhand von Daten aus der Datenbank</li>
+                            <li>Sortierung von Produkten nach mindestens 3 Parametern, Verknüpfung mit UND oder ODER
+                            </li>
+                            <li>vorhandene Nutzeraccounts</li>
+                            <li>Nutzerregistrierung</li>
+                            <li>Fehlerbehandlung für nicht bekannte Seiten (404)</li>
+                            <li>Dynamisches Nachladen von Inhalten mittels JavaScript, wo sinnvoll</li>
+                        </ul>
+                    </li>
+                </ul>
+                <h4>Wunsch-Kriterien</h4>
+                <ul>
+                    <li>
+                        GWP
+                        <ul>
+                            <li>Animationen und Überblendungen für angenehme User Experience</li>
+                            <li>Stilangaben für Druckausgabe (z.B. Bestellbestätigung, Produktübersicht)</li>
+                        </ul>
+                    </li>
+                    <li>
+                        DWP
+                        <ul>
+                            <li>asynchrones Absenden von Formularen mittels AJAX</li>
+                        </ul>
+                    </li>
+                </ul>
             </li>
             <li>
                 <h3>Zu liefernde Funktionalitäten (inkl. zugehörigem Entwickler)</h3>
+
             </li>
             <li>
                 <h3>Webseitenstruktur</h3>
-                <a class="websiteDiagramFull" href="/diagrams/ProtecDBV1.1.png" target="_blank" ><img class="websiteDiagram" src="/diagrams/ProtecDBV1.1.png" alt="Datenbankdiagramm"></a>
+                <a class="websiteDiagramFull" href="/diagrams/WebsiteStruktur.png" target="_blank" ><img class="websiteDiagram" src="/diagrams/WebsiteStruktur.png" alt="Datenbankdiagramm"></a>
             </li>
             <li>
                 <h3>Datenbankstruktur</h3>
