b75325067504ae2d894423a88fa7764cc574e12f
Thom-Mon <thomas.gebel@fh-erfurt.de>
2021-02-18 17:50:55 +0100

    Suchfilter verbessert mit Get

--- views/pages/search.php
+++ views/pages/search.php
@@ -8,14 +8,20 @@
     <?foreach($products as $element) : ?>
         <?if(in_array($element->category,$categoriesInSearch)){}else{array_push($categoriesInSearch,$element->category);}  ?>
     <?endforeach;?> 
-    <?foreach($categoriesInSearch as $element) : ?>
-    <input class="categorieFilter" type="checkbox" name="<?=$element?>"><?=$element?><br>
-    <?endforeach;?>
-    <form method="post">
+
+    <form method="get">
+        <input type="hidden" name="c" value="pages" >  
+        <input type="hidden" name="a" value="search">  
+        <?foreach($categoriesInSearch as $element) : ?>
+        <input class="categorieFilter" type="checkbox" name="<?=$element?>" <?=isset($_GET[$element]) ? 'checked' : ''?>><?=$element?><br>
+        <?endforeach;?>
+    
+        <label for="searchString">Suchbegriff</label>  
+        <input type="text" name="searchString"<?if(isset($_GET['searchString'])){echo "value=".htmlspecialchars($_GET['searchString']);};?>><br>
         <label for="minPrice">Preis</label>  
         <input type="text" name="minPrice" placeholder="von">
         <input type="text" name="maxPrice" placeholder="bis"><br>
-        <button type="submit" name="searchString">Ergebnisse filtern</button><br>
+        <button type="submit" >Ergebnisse filtern</button><br>
     </form>
 </div>
 
@@ -26,11 +32,12 @@
 
        
         <h2>Ihre Suchergebnisse</h2>
+        <?=isset($filteredProducts) ? count($filteredProducts)." Treffer" : "";?>
+
         <div class="contentContainer">
             <div class="subcategoryContent">
-            <?if(isset($products) && !empty($products)): ?>
-                
-                <?foreach($products as $element) : ?>
+            <?if(isset($filteredProducts) && !empty($filteredProducts)): ?>
+                <?foreach($filteredProducts as $element) : ?>
                     <div class="element">
                         <a href="index.php?c=products&a=product&pid=<?=$element->productID?>">
                         <img src="<?=IMAGESPATH?><?=$element->productID?>.png"></a>
@@ -45,14 +52,15 @@
                     <img src="<?=IMAGESPATH?>sadrobot1.png" alt="sadrobot"><br>
                     <p>Halb so schlimm...starten Sie einfach eine neue Suche...</p><br>
                     <form method="POST">
-                    <input type="text" name="searchString" placeholder="neue Suche..."<?if(isset($_POST['searchString'])){echo "value=".htmlspecialchars($_POST['searchString']);};?>>
+                    <input type="text" name="searchString" placeholder="neue Suche..."<?if(isset($_GET['searchString'])){echo "value=".htmlspecialchars($_GET['searchString']);};?>>
                     </form>
                 </div>
             <?endif;?>
-            
-            
+                 
             </div>
         </div>
         </div>
+                    <p><?echo $_GET['searchString']?> </p>
+                    <p><?=$_SERVER['QUERY_STRING']?></p>
     </main>
 </body>
