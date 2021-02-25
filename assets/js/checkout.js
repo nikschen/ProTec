ea3d7b10af93e325ea82d7e5958c7992c93065ff
Thom-Mon <thomas.gebel@fh-erfurt.de>
2021-02-25 10:28:59 +0100

    checkout gesäubert

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,15 +1,11 @@
-
-
 function hidePaymentNumber() {
-        console.log("klappt doch")
-        document.getElementById('paymentNumber').setAttribute("style","display:none");
-        document.getElementById('paymentNumber').required=false;
+    document.getElementById('paymentNumber').setAttribute("style", "display:none");
+    document.getElementById('paymentNumber').required = false;
 
 }
 
 function showPaymentNumber() {
-    console.log("klappt doch")
-    document.getElementById('paymentNumber').setAttribute("style","display:block");
-    document.getElementById('paymentNumber').required=true;
+    document.getElementById('paymentNumber').setAttribute("style", "display:block");
+    document.getElementById('paymentNumber').required = true;
 
 }
