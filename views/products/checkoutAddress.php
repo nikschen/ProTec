e1b252caba3a4c350ecb7a2e165944aa6ade15a4
nikschen <niklaswiemuth@gmail.com>
2021-02-25 09:08:15 +0100

    keine ahnung was hier groß anders sein soll

--- views/products/checkoutAddress.php
+++ views/products/checkoutAddress.php
@@ -13,12 +13,12 @@
                         <p> Ihre Lieferadresse</p>
                         <br>
                         <input type="text" name="firstNameShipping" placeholder="Ihr Vorname*" value="<?if (isset($_POST['firstNameShipping'])){echo htmlspecialchars($_POST['firstNameShipping']);}else echo $firstNameShippingValue?>" required >
-                        <input type="text" name="lastNameShipping" placeholder="Ihr Name*" value="<?if (isset($_POST['lastNameShipping'])){echo htmlspecialchars($_POST["lastNameShipping"]);}else echo $lastNameShippingValue?>" required ><br>
+                        <input type="text" name="lastNameShipping" placeholder="Ihr Name*" value="<?if (isset($_POST['lastNameShipping'])){echo htmlspecialchars($_POST['lastNameShipping']);}else echo $lastNameShippingValue?>" required ><br>
                         <input class="oneLine" type="text" name="streetShipping" placeholder="Straße*" value="<?if (isset($_POST['streetShipping'])){echo htmlspecialchars($_POST['streetShipping']);}else echo $streetShippingValue?>" required >
                         <input class="oneLine" type="text" name="streetNoShipping" placeholder="Hausnummer*" value="<?if (isset($_POST['streetNoShipping'])){echo htmlspecialchars($_POST['streetNoShipping']);}else echo $streetNoShippingValue?>" required ><br>
                         <input class="oneLine" type="text" name="additionalInformation" id="additionalInformation" placeholder="Adresszusatz" value="<?if (isset($_POST['additionalInformation'])){echo htmlspecialchars($_POST['additionalInformation']);}else echo $additionalInformationValue?>"><br>
                         <input class="oneLine" type="text" name="zipcodeShipping" placeholder="PLZ*" value="<?if (isset($_POST['zipcodeShipping'])){echo htmlspecialchars($_POST['zipcodeShipping']);}else echo $zipcodeShippingValue?>" required >
-                        <input class="oneLine" type="text" name="cityShipping" placeholder="Ort*" value="<?if (isset($_POST['cityShipping'])){echo htmlspecialchars($_POST['cityShipping']);}else echo $cityShippingValue?>" required ><br>
+                        <input class="oneLine" type="text" name="cityShipping" placeholder="Ort*" value="<?if (isset($_POST['cityShipping'])){echo $_POST['cityShipping'];}else echo $cityShippingValue?>" required ><br>
                         <input type="text" name="countryShipping" id="countryShipping" placeholder="Land*" value="<?if (isset($_POST['countryShipping'])){echo htmlspecialchars($_POST['countryShipping']);}else echo $countryShippingValue?>" required ><br>
                         <input type="text" class="emailToCheck" id="email" name="emailShipping" placeholder="Ihre E-Mail-Adresse*"  onchange='checkEmail()' value="<?if (isset($_POST['emailShipping'])){echo htmlspecialchars($_POST['emailShipping']);}else echo $emailShippingValue?>" required >
                         <p class="messageMail" ></p>
@@ -34,7 +34,7 @@
                         <p> Ihre Rechnungsadresse</p>
                         <br>
                         <input type="text" name="firstNameBilling" placeholder="Ihr Vorname*" value="<?if (isset($_POST['firstNameBilling'])){echo htmlspecialchars($_POST['firstNameBilling']);}else echo $firstNameBillingValue?>" required >
-                        <input type="text" name="lastNameBilling" placeholder="Ihr Name*" value="<?if (isset($_POST['lastNameBilling'])){echo htmlspecialchars($_POST["lastNameBilling"]);}else echo $lastNameBillingValue?>" required ><br>
+                        <input type="text" name="lastNameBilling" placeholder="Ihr Name*" value="<?if (isset($_POST['lastNameBilling'])){echo htmlspecialchars($_POST['lastNameBilling']);}else echo $lastNameBillingValue?>" required ><br>
                         <input class="oneLine" type="text" name="streetBilling" placeholder="Straße*" value="<?if (isset($_POST['streetBilling'])){echo htmlspecialchars($_POST['streetBilling']);}else echo $streetBillingValue?>" required >
                         <input class="oneLine" type="text" name="streetNoBilling" placeholder="Hausnummer*" value="<?if (isset($_POST['streetNoBilling'])){echo htmlspecialchars($_POST['streetNoBilling']);}else echo $streetNoBillingValue?>" required ><br>
                         <input class="oneLine" type="text" name="zipcodeBilling" placeholder="PLZ*" value="<?if (isset($_POST['zipcodeBilling'])){echo htmlspecialchars($_POST['zipcodeBilling']);}else echo $zipcodeBillingValue?>" required >
